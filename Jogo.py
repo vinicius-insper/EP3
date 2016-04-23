0decd038ef00cac39719ec67be113f4abcaa359a
vinicius-insper <viniciusbf1@al.insper.edu.br>
2016-04-23 18:00:18 -0300

    criei a funcao verifica ganhador

--- Jogo.py
+++ Jogo.py
@@ -15,3 +15,22 @@ class jogo():
 			self.matriz[linha][coluna] = 5
 			self.turno = 1
 			self.rodada += 1
+
+	def verifica_ganhador(self):
+		a = self.matriz[0][0] + self.matriz[0][1] + self.matriz[0][2]
+		b = self.matriz[1][0] + self.matriz[1][1] + self.matriz[1][2]
+		c = self.matriz[2][0] + self.matriz[2][1] + self.matriz[2][2]
+		d = self.matriz[0][0] + self.matriz[1][0] + self.matriz[2][0]
+		e = self.matriz[0][1] + self.matriz[1][1] + self.matriz[2][1]
+		f = self.matriz[0][2] + self.matriz[1][2] + self.matriz[2][2]
+		g = self.matriz[0][0] + self.matriz[1][1] + self.matriz[2][2]
+		h = self.matriz[2][0] + self.matriz[1][1] + self.matriz[0][2]
+
+		if a == 3 or b == 3 or c == 3 or d == 3 or e == 3 or f == 3 or g == 3 or h == 3:
+			return 2
+		elif a == 15 or b == 15 or c == 15 or d == 15 or e == 15 or f == 15 or g == 15 or h == 15:
+			return 1
+		elif self.rodada == 9:
+			return 0
+		else:
+			return -1
