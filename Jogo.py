7e22ad544731a36acae6eafcaa290eea68f4ef0c
vinicius-insper <viniciusbf1@al.insper.edu.br>
2016-04-23 17:59:18 -0300

    criei funcao recebe jogada

--- Jogo.py
+++ Jogo.py
@@ -5,3 +5,13 @@ class jogo():
 		self.matriz = matrizzeros
 		self.turno = turno
 		self.rodada = rodada
+
+	def recebe_jogada(self, linha, coluna):
+		if self.turno == 1:
+			self.matriz[linha][coluna] = 1
+			self.turno = 2
+			self.rodada += 1
+		elif self.turno == 2:
+			self.matriz[linha][coluna] = 5
+			self.turno = 1
+			self.rodada += 1
